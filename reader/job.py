075430dfc366d98b4a4db27d340c1bc5704ed1b0
arnaud-soulie <soulie.arnaud@gmail.com>
2020-08-04 21:19:18 +0200

    init

---
+++ reader/job.py
@@ -0,0 +1,40 @@
+""" Talend job abstraction
+"""
+import os
+
+class Job:
+    """ Talend single job abstraction
+    Provides methods to read items files to retrieve job properties
+    """
+
+    def __init__(self, *, item_path, table_cible, table_source, schema_source,
+                 tree="", zone, root_path):
+        self.item_path = item_path
+        self.table_cible = table_cible
+        self.table_source = table_source
+        self.schema_source = schema_source
+        self.root_path = root_path
+        self.tree = self.__get_tree_from_path()
+        self.job_name = self.get_name_from_path()
+        self.zone = zone
+
+    def get_name_from_path(self):
+        """Get the job name from job filepath
+
+        Returns:
+            string: job name
+        """
+        assert self.item_path != ""
+        #Removes the _0 at the end of each filename
+        return os.path.basename(self.item_path).split('.')[0][:-2]
+
+    def __get_tree_from_path(self):
+        assert self.item_path != ""
+        rel_path = os.path.relpath(self.item_path, start=os.path.join(self.root_path, "process"))
+        return os.path.dirname(rel_path)
+
+
+    def __str__(self):
+        return f"Job: {self.job_name}, TB_Cible: {self.table_cible}, "\
+            f"TB_Source: {self.table_source}, Schema: {self.schema_source}, "\
+                f"Zone: {self.zone}, Tree: {self.tree}"
---
+++ reader/project_parser.py
@@ -0,0 +1,85 @@
+""" Parse a Talend project to retrieve all the different job properties
+
+    Raises:
+        Exception: Talend root directory not found
+        Exception: Talend process directory empty
+"""
+
+import glob
+import os
+import re
+import xml.etree.ElementTree as ET
+
+from reader.job import Job
+#from job import Job
+
+
+class ProjectParser:
+    """ Talend project parser retrieves job properties and provide an output prop list
+    """
+
+    def __init__(self, _project_path):
+        """ Parser init
+        Args:
+            _project_path (string): path to talend project root directory
+        """
+        self._project_path = _project_path
+        self.job_data_list = []
+        self.__check_project()
+
+    def __check_project(self):
+        """Check that the talend project contains a non-empty process director
+
+        Raises:
+            Exception: Talend root directory not found
+            Exception: Talend process directory empty
+        """
+        self.process_dir = os.path.join(self._project_path, "process")
+        if not os.path.exists(self.process_dir):
+            raise Exception("Directory {} does not exist".format(self.process_dir))
+        if os.listdir(self.process_dir) == 0:
+            raise Exception("Directory {} is empty".format(self.process_dir))
+
+    def parse_jobs(self):
+        """ Creates a list of talend jobs available in the project
+        """
+        job_list = glob.glob(os.path.join(self.process_dir, "**\\*.item"), recursive=True)
+        for job in job_list:
+            processed_job = self.__process_job_item(job)
+            self.job_data_list.append(processed_job)
+
+    def __process_job_item(self, item_path):
+        tree = ET.parse(item_path)
+        root = tree.getroot()
+        try:
+            desc = root.findall(".//node/[@componentName='tJava']/elementParameter/" \
+                "[@field='MEMO_JAVA']")[0].attrib["value"]
+        except (IndexError, KeyError):
+            print(f"tJava/MEMO_JAVA/value not found in the XML tree for {item_path}")
+            return
+        table_cible = self.__get_field_value_from_desc(desc, "context.journal_tb_cible")
+        table_source = self.__get_field_value_from_desc(desc, "context.journal_tb_source")
+        schema_source = self.__get_field_value_from_desc(desc, "context.journal_source")
+        zone = self.__get_field_value_from_desc(desc, "context.journal_zone")
+        return Job(item_path=item_path, table_cible=table_cible, table_source=table_source,
+                   schema_source=schema_source, zone=zone, root_path=self._project_path)
+
+
+    def __get_field_value_from_desc(self, desc, field):
+        data_list = desc.split("\n")
+        for line in data_list:
+            line_data = line.split("=")
+            if (field == line_data[0].strip()) and (len(line_data) > 1):
+                try:
+                    value = re.search('"(.+?)"', line_data[1]).group(1)
+                    return value
+                except AttributeError:
+                    return ""
+
+
+
+if __name__ == "__main__":
+    parser = ProjectParser("D:\\Programmation\\Preci\\sari_lot1-v2.0")
+    parser.parse_jobs()
+    for j in parser.job_data_list:
+        print(j)
