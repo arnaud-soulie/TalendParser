aa8c99b3a7d0d3eaa5c5400cc9bb2f56dd14ce53
arnaud-soulie <soulie.arnaud@gmail.com>
2020-08-11 13:45:37 +0200

    Switch to Qt5

--- package.bat
+++
@@ -1,4 +0,0 @@
-PyInstaller -y --add-data "main.kv;."  --hiddenimport pkg_resources.py2_warn --hiddenimport win32timezone gui.py
-echo Looking for libpng16-16.dll in current venv
-copy %VIRTUAL_ENV%\share\sdl2\bin\libpng16-16.dll .\dist\gui\libpng16-16.dll
-.\dist\gui\gui.exe
