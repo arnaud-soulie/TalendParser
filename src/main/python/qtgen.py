a39db9be09e79d1ba789973eecbc49a818fb9f7e
arnaud-soulie <soulie.arnaud@gmail.com>
2020-08-11 14:47:39 +0200

    Update gui

--- src/main/python/qtgen.py
+++ src/main/python/qtgen.py
@@ -11,11 +11,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(541, 439)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+class Ui_TalendDocGenerator(object):
+    def setupUi(self, TalendDocGenerator):
+        TalendDocGenerator.setObjectName("TalendDocGenerator")
+        TalendDocGenerator.resize(541, 439)
+        self.centralwidget = QtWidgets.QWidget(TalendDocGenerator)
         self.centralwidget.setObjectName("centralwidget")
         self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.formLayoutWidget.setGeometry(QtCore.QRect(10, 10, 521, 381))
@@ -69,44 +69,43 @@ class Ui_MainWindow(object):
         self.ReportButton = QtWidgets.QPushButton(self.formLayoutWidget)
         self.ReportButton.setMinimumSize(QtCore.QSize(0, 40))
         self.ReportButton.setObjectName("ReportButton")
-        self.ReportButton.setEnabled(False)
         self.formLayout.setWidget(4, QtWidgets.QFormLayout.SpanningRole, self.ReportButton)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        TalendDocGenerator.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(TalendDocGenerator)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 541, 22))
         self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        TalendDocGenerator.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(TalendDocGenerator)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        TalendDocGenerator.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(TalendDocGenerator)
+        QtCore.QMetaObject.connectSlotsByName(TalendDocGenerator)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, TalendDocGenerator):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Talend project root path:"))
-        self.projectRootPath.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        TalendDocGenerator.setWindowTitle(_translate("TalendDocGenerator", "Talend Doc Generator"))
+        self.label.setText(_translate("TalendDocGenerator", "Talend project root path:"))
+        self.projectRootPath.setHtml(_translate("TalendDocGenerator", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">D:\\Programmation\\Preci\\sari_lot1-v2.0</p></body></html>"))
-        self.label_3.setText(_translate("MainWindow", "Report Directory:"))
-        self.reportPath.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.label_3.setText(_translate("TalendDocGenerator", "Report Directory:"))
+        self.reportPath.setHtml(_translate("TalendDocGenerator", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">D:\\</p></body></html>"))
-        self.GenerateButton.setText(_translate("MainWindow", "Generate"))
-        self.ReportButton.setText(_translate("MainWindow", "Report"))
+        self.GenerateButton.setText(_translate("TalendDocGenerator", "Generate"))
+        self.ReportButton.setText(_translate("TalendDocGenerator", "Report"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    TalendDocGenerator = QtWidgets.QMainWindow()
+    ui = Ui_TalendDocGenerator()
+    ui.setupUi(TalendDocGenerator)
+    TalendDocGenerator.show()
     sys.exit(app.exec_())
